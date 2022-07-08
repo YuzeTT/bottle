6f2d3d9e3c0b9a63177ba07105e54e43515cb308
YuzeTT <ziv1024@163.com>
2022-07-08 16:34:50 +0800

    chore: set font

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import animated  from'animate.css'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(router)
+.use(animated)
+.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Index',
+    component: () => import('../views/Index/Index.vue')
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
